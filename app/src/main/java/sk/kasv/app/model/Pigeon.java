6b7888bdb0cfde36b6adcd332f448998b4489a53
Tomáš Tejiščák <tomas.tejiscak@gmail.com>
2024-11-29 09:25:19 +0100

    dopísal som getteri, setteri a cons do modelov entít

--- app/src/main/java/sk/kasv/app/model/Pigeon.java
+++ app/src/main/java/sk/kasv/app/model/Pigeon.java
@@ -6,4 +6,56 @@ public class Pigeon {
     private String color;
     private String breed;
     private int addedByUserId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAddedByUserId() {
+        return addedByUserId;
+    }
+
+    public void setAddedByUserId(int addedByUserId) {
+        this.addedByUserId = addedByUserId;
+    }
+
+    public Pigeon() {
+
+    }
+
+    public Pigeon(int id, String name, String color, String breed, int addedByUserId) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.breed = breed;
+        this.addedByUserId = addedByUserId;
+    }
 }
