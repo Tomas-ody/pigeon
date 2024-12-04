f3430e72a7636197c02eda271e3d434a0b296fb7
Róbert Lumcer <robert.lumcer@kosickaakademia.sk>
2024-12-04 08:41:21 +0100

    initial

---
+++ Pigeon/src/app/pigeon/family-tree/family-tree.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-family-tree',
+  imports: [],
+  templateUrl: './family-tree.component.html',
+  styleUrl: './family-tree.component.css'
+})
+export class FamilyTreeComponent {
+
+}
