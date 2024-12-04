5e54f5a083081d6d9e7b49e01da30417750a45f8
Róbert Lumcer <robert.lumcer@kosickaakademia.sk>
2024-12-04 08:17:18 +0100

    Initial commit for Pigeon project

---
+++ Pigeon/src/app/components/pigeon-edit/pigeon-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pigeon-edit',
+  imports: [],
+  templateUrl: './pigeon-edit.component.html',
+  styleUrl: './pigeon-edit.component.css'
+})
+export class PigeonEditComponent {
+
+}
---
+++ Pigeon/src/app/components/pigeon-list/pigeon-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pigeon-list',
+  imports: [],
+  templateUrl: './pigeon-list.component.html',
+  styleUrl: './pigeon-list.component.css'
+})
+export class PigeonListComponent {
+
+}
