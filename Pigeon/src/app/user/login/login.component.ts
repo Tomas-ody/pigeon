3953463962f5f9c3a6fcf477d05fd54d505868e2
Tomáš Tejiščák <tomas.tejiscak@gmail.com>
2024-12-13 08:51:22 +0100

    login works

--- Pigeon/src/app/user/login/login.component.ts
+++ Pigeon/src/app/user/login/login.component.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { Auth } from '../../pigeon/entities/auth';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   constructor(private authService: AuthService) {}
+  
+  auth: Auth = new Auth("admin","admin");
+  usersService = inject(UserService);
+  router = inject(Router);
 
-  login() {
-    const success = this.authService.login("user1", "user1");
-    if (success) {
-      console.log("Login successful");
-    }
-    else {
-      console.log("Login failed");
-    }
+
+  onSubmit() {
+    //odoslať auth ako rest request
+    this.usersService.login(this.auth).subscribe({
+      next: success => {
+        if (success) {
+          // idem na autorizovanu sekciu
+          this.router.navigateByUrl(this.usersService.navigateAfterLogin);
+        } 
+      }
+    });
   }
 }
