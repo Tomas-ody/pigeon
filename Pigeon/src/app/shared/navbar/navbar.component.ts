5f4861c529daba4fd4156f4f6e52e464b1e77fa1
Róbert Lumcer <robert.lumcer@kosickaakademia.sk>
2024-12-13 11:25:48 +0100

    progress

--- Pigeon/src/app/shared/navbar/navbar.component.ts
+++ Pigeon/src/app/shared/navbar/navbar.component.ts
@@ -7,6 +7,17 @@ import { MaterialModule } from '../../modules/material.module';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+/*import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { PigeonComponent } from './Pigeon/pigeon';
+import { LogoutComponent } from './logout/logout.component';
+
+const routes: Routes = [
+    { path: 'profile', component: ProfileComponent },
+    { path: 'pigeon', component: PigeonComponent },
+    { path: 'logout', component: LogoutComponent },
+    { path: '', redirectTo: '/profile', pathMatch: 'full' } // Default route
+];*/
 export class NavbarComponent {
 
 }
