50aa227147c80eae963ec7498642334d8f4337db
Tomáš Tejiščák <tomas.tejiscak@gmail.com>
2025-01-23 10:46:36 +0100

    hotfix, service<=>service injected

--- Pigeon/src/app/shared/errorHandler.service.ts
+++ Pigeon/src/app/shared/errorHandler.service.ts
@@ -3,7 +3,6 @@ import { inject, Injectable } from "@angular/core";
 import { EMPTY, Observable } from "rxjs";
 import { MessageService } from "./message.service";
 import { Router } from "@angular/router";
-import { UserService } from "../user/user.service";
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +11,6 @@ export class ErrorHandler {
 
 messageService = inject(MessageService);
 router = inject(Router);
-userService = inject(UserService);
 
 errorHandling(err: any, message?: string):Observable<never> {
     if (err instanceof HttpErrorResponse) {
@@ -23,7 +21,7 @@ errorHandling(err: any, message?: string):Observable<never> {
       if (err.status >= 400 && err.status < 500) {
         const msg = err.error.errorMessage || JSON.parse(err.error).errorMessage;
         if (msg === "unknown token") {
-          this.userService.token = '';
+          localStorage.removeItem("Token");
           this.messageService.errorToast("Session lost, please log in again", 'X', 2000);
           this.router.navigateByUrl("/login");
           return EMPTY;
